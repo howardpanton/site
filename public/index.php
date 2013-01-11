7881e2be2fb8e748b7ab061b17060956c26cbae3
howardpanton <howardpanton@gmail.com>
2013-01-11 12:32:43 +0000

    updated boxfile

--- public/index.php
+++ public/index.php
@@ -1,10 +1,10 @@
 <?php
  echo dirname(__FILE__);
-    require_once '/var/www/system/Templater.php';
+    require_once '_system/Templater.php';
 
     /*Here we setup the most simple usage of the template*/
 
-    $page = new Templater("/var/www/templates/main.tpl.php");    // Loading the template file
+    $page = new Templater("_templates/main.tpl.php");    // Loading the template file
     /*Setting variables using the 2 methods*/
     $page->title = "Home Page";
     /* Load the current directory into an array*/
