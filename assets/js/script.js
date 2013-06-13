92390f23232c708140521deb1d1a49f5e941606a
prichardson <pete.richardson82@gmail.com>
2013-06-13 12:16:52 +0100

    add dave rupert's fitHeight plugin to script.js -used to equalise height of list elements with different amounts of content inside them

--- assets/js/script.js
+++ assets/js/script.js
@@ -636,6 +636,54 @@ $(window).resize(function () {
 
 
 
+/* 
+jQuery.fitHeights by Paravel™
+
+Author: Dave Rupert
+Author URL: http://daverupert.com/
+Based on: https://github.com/filamentgroup/jQuery-Equal-Heights 
+
+*/
+(function(){
+  
+  $.fn.fitHeights = function() {
+    
+    var items = $(this);
+    function setHeights() {
+      
+      var currentTallest = 0;
+  
+      items.css({ 'min-height' : currentTallest });  // unset min-height to get actual new height
+
+      // right now this causes a noticeable shift in height on resize. workarounds?
+      
+      items.each(function(){
+        
+        if( $(this).height() > currentTallest ) { currentTallest = $(this).height(); }
+      });
+
+      items.css({ 'min-height' : currentTallest });
+    
+    }
+    
+    setHeights();
+    $(window).on('resize', setHeights);
+    return this;
+  };
+})(jQuery);
+
+$(window).load(function(){
+  /* $(groupOfItems).fitHeights(); */
+  $('ul li').fitHeights();
+});
+
+
+
+
+
+
+
+
 // jquery jpanel slide from left menu for docs page 
 
 /**
