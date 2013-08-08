ef3fc020e3437b613fdae9084c11df96e332f28c
Alastair Mucklow <amucklow@strangerpixel.com>
2013-08-08 15:31:02 +0100

    Implement carousel template and tidy

--- assets/js/script.js
+++ assets/js/script.js
@@ -401,21 +401,21 @@ $(document).ready(function(){
 
   // detect slider component
   
-  if ($('.js-slider').length > 0) {
+  if ($('.js-carousel').length > 0) {
 
     $.getScript('http://artslondon.github.io/beta/assets/js/components/jquery.bxslider.min.js', function() {
 
-      $.each($('.js-slider'), function() {
+      $.each($('.js-carousel'), function() {
 
         var _this = $(this);
         var _wrapper = _this.closest('.bx-wrapper'); // the .bx-wrapper container div
 
         // get the individual slide width from the data-slider-item-width value in the HTML. If there's nothing set in the data-attribute, set the width to 0 - i.e. max-width
-        var _itemWidth = (_this.data('slider-item-width') > 0) ? _this.data('slider-item-width') : 0;
+        var _itemWidth = (_this.data('carousel-item-width') > 0) ? _this.data('carousel-item-width') : 0;
         // set the minimum number of slides before it starts to be responsive
-        var _itemMinSlides = (_this.data('slider-min-slides') > 0) ? _this.data('slider-min-slides') : 0;
+        var _itemMinSlides = (_this.data('carousel-min-slides') > 0) ? _this.data('carousel-min-slides') : 0;
         // get the margin between slides from the data-slider-item-margin value in the HTML. If there's nothing set in the data-attribute, set the margin to 0
-        var _itemMargin = (_this.data('slider-item-margin') > 0) ? _this.data('slider-item-margin') : 0;
+        var _itemMargin = (_this.data('carousel-item-margin') > 0) ? _this.data('carousel-item-margin') : 0;
         // slider instances always show next/prev controls, unless data-controls is false          
         var _controlsOpt = true;
         _controlsOpt = _this.data('controls');
