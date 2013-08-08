a8d8bd83da7ec403f1f4116eed539add3a6c1564
wisbey <wisbey1@gmail.com>
2013-08-08 17:56:14 +0100

    Created LCC bespoke components vertical image profile accordion and restyled tabs component for LCC
    added, mousewheel.js and vaccordion.js

--- assets/js/script.js
+++ assets/js/script.js
@@ -771,6 +771,24 @@ if ($('.accordion').length > 0) {
 }
   
 
+// detect vertical accordion component
+if ($('#va-accordion').length > 0) {
+  $.when(
+    $.getScript( "http://artslondon.github.io/beta/assets/js/components/jquery.easing.1.3.js" ),
+    $.getScript( "http://artslondon.github.io/beta/assets/js/components/jquery.mousewheel.js" ),
+    $.getScript( "http://artslondon.github.io/beta/assets/js/components/jquery.vaccordion.js" ),
+    $.Deferred(function( deferred ){
+      $( deferred.resolve );
+    })
+  ).done(function(){
+  //place your code here, the scripts are all loaded
+    $('#va-accordion').vaccordion();
+  }); 
+}
+
+
+
+
 // detect dropdown menu button
 if ($('.dd-menu').length > 0) {
 
