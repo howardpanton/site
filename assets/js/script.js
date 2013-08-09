a855905d075da32835a8c6def05531555c944337
denovo <pete.richardson82@gmail.com>
2013-08-09 15:03:07 +0100

    fix accordion
    text inside <p> tags within accordion now spans max 8 cols,
    
    icons now rotate in fixed positon

--- assets/js/script.js
+++ assets/js/script.js
@@ -531,18 +531,28 @@ if ($('.accordion').length > 0) {
 
     });
 
+    function resetSpinners() {
+      // check if there are any other open accordion items, and close them if so
+      $( ".accordion-list-item" ).each(function (e) {
+        var _li_item = $(this); 
+        if ( _li_item.hasClass('st-open') ) {
+            _li_item.find('.st-arrow').rotate({animateTo:0, center: ["50%", "50%"], });
+        }
+      });
+    }
+
     $(".accordion-list-anchor").on("click", ".size-h4", function(event){
         var circle = ($(this).next('.st-arrow'));
+        var accordion = ($(this).parent().parent());
         var elem = ($(this).parent().next('.st-content'));
-        console.log(circle);
-              //circle.rotate({animateTo:360});
+              resetSpinners();
+              // $(this).scrollToMe(); // scroll to the clicked elem
               if (!elem.is(':visible'))  {
                 circle.rotate({animateTo:135});
                } else {
                 circle.rotate({animateTo:0, center: ["50%", "50%"], });
-            };
-
-});
+              };
+    });
 
 }
   
