df92f86179a0b3cbaa55526790cbab1bc3350150
howardpanton <howardpanton@gmail.com>
2013-08-13 17:14:06 +0100

    Updates course Class changes

--- _php/course-class.php
+++ _php/course-class.php
@@ -1,20 +1,24 @@
 <?php
 
-if(class_exists('Block') != true) 
+if(class_exists('Course') != true) 
 {
 	
 	class Course {
     		// Create course object
     		public function __construct(Array $properties = array()) {
 	
-		function odd($properties) {
+//		public static function get_college($query) {
+//			parse_str($_SERVER['QUERY_STRING']);
+//		}
+//	
+		function set_college($properties) {
 		    // returns whether the input integer is odd
 		    return($properties['college'] == "London College of fashion");
 		}
 
 
-		$test = array_filter($properties, "odd");
-		print_r($test);
+		$test = array_filter($properties, "set_college");
+		//print_r($test);
 
       		foreach($test as $key => $value) {
         		$this->{$key} = $value;
@@ -66,6 +70,16 @@ $courses[] = array(
 "usp"  => "Aenean lacinia bibendum nulla sed consectetur. Integer posuere erat a ante venenatis dapibus posuere velit aliquet. Fusce dapibus, tellus ac cursus commodo, tortor mauris condimentum nibh, ut fermentum massa justo sit amet risus."
 );
 
+
+$courses[] = array( 
+"title" => "Ipsum Porta Inceptos Parturient Euismod",
+"level"  => "Undergraduate",
+"college" => "London College of fashion",
+"mode"  => "Full time",
+"url" => "http://www.arts.ac.uk/fashion/courses/undergraduate/ba-fashion",
+"usp"  => "Aenean lacinia bibendum nulla sed consectetur. Integer posuere erat a ante venenatis dapibus posuere velit aliquet. Fusce dapibus, tellus ac cursus commodo, tortor mauris condimentum nibh, ut fermentum massa justo sit amet risus."
+);
+
 $courses[] = array( 
 "title" => "Ipsum Porta Inceptos Parturient Euismod",
 "level"  => "Undergraduate",
@@ -77,6 +91,8 @@ $courses[] = array(
 
   
 $course = new Course($courses);
+
+parse_str($_SERVER['QUERY_STRING']);
 
 // output course array in JSON
 echo json_encode($course);
