822c19ec577d44654f947a3e149678ffa2afad3a
Alastair Mucklow <amucklow@strangerpixel.com>
2013-09-11 17:20:11 +0100

    Add KIS widget code to main script.js file

--- assets/js/script.js
+++ assets/js/script.js
@@ -805,7 +805,18 @@ $('.l-content a[href*="github."]').addClass('github-link');
 
 
 
-
+// KIS WIDGET
+if ($('.kis-widget').length > 0) {
+  (function (d) {
+  "use strict";
+  var widgetScript = d.createElement('script'); 
+  widgetScript.id = 'unistats-widget-script';
+      widgetScript.src = '//widget.unistats.ac.uk/js/unistats.widget.js';
+  var scriptTags = d.getElementsByTagName('script')[0];
+  if (d.getElementById('unistats-widget-script')) {  return; } 
+  scriptTags.parentNode.insertBefore(widgetScript, scriptTags);
+  } (document));
+}
 
 
 
