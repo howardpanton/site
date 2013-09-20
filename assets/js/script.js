f34ebf7a67147a5f39b5dc9cb8204392bf8cad18
howardpanton <howardpanton@gmail.com>
2013-09-20 13:31:49 +0100

    merging fixes

--- assets/js/script.js
+++ assets/js/script.js
@@ -15,12 +15,32 @@ jQuery.fn.extend({
 // --------------------------------------------------
 
 
+// add indexOf for IE8 compatibility
+if (!Array.prototype.indexOf) {
+    Array.prototype.indexOf = function (elt /*, from*/) {
+        var len = this.length >>> 0;
+        var from = Number(arguments[1]) || 0;
+        from = (from < 0) ? Math.ceil(from) : Math.floor(from);
+        if (from < 0) from += len;
+
+        for (; from < len; from++) {
+            if (from in this && this[from] === elt) return from;
+        }
+        return -1;
+    };
+}
+
+
+
 $(".date").each(function (i, element) {
   
   str = $( this ).text();
+  if (str.indexOf(",") != -1) {
+    $(this).text(str.substring(5,16));
+  }
 
-  $(this).text(str.substring(5,16));
 });
+
 
 
 
