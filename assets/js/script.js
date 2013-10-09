8b603e250b0689923ed43f03ca5b99806517078f
denovo <pete.richardson82@gmail.com>
2013-10-09 13:51:23 +0100

    add fix for mobile iOS re-orientation bug

--- assets/js/script.js
+++ assets/js/script.js
@@ -988,3 +988,19 @@ $(window).load(function(){
   }
 
 });
+
+
+// Fix iOS re-orient problem when changing from portrait to landscape mode
+// The script below will make sure that the screen width is updated correctly 
+window.document.addEventListener('orientationchange', function() {
+  var iOS = navigator.userAgent.match(/(iPad|iPhone|iPod)/g);
+  var viewportmeta = document.querySelector('meta[name="viewport"]');
+  if (iOS && viewportmeta) {
+    if (viewportmeta.content.match(/width=device-width/)) {
+      viewportmeta.content = viewportmeta.content.replace(/width=[^,]+/, 'width=1');
+    }
+    viewportmeta.content = viewportmeta.content.replace(/width=[^,]+/, 'width=' + window.innerWidth);
+  }
+  // If you want to hide the address bar on orientation change, uncomment the next line
+  window.scrollTo(0, 0); 
+}, false);
