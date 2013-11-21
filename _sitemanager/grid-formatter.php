11b9f6454ac29c1fce1321d3437240f1354c7bf0
Alastair Mucklow <amucklow@strangerpixel.com>
2013-11-21 17:15:38 +0000

    Site Manager stuff

--- _sitemanager/grid-formatter.php
+++ _sitemanager/grid-formatter.php
@@ -5,9 +5,12 @@
 - Text Block with Aside
 - Panorama: HTML
 -  
+- See also:
+- Content Gallery Image (which uses the same code in a text/html formatter)
+-
 -->  
 <?php 
-/* formatter for displaying list children at a parent level, in a content grid */
+// formatter for displaying list children at a parent level, in a content grid
 if (!isset($old_section_title)) { $old_section_title = ''; }
 $media_path = '';
 $thumbnail_media_path = "<t4 type="content" name="Optional Thumbnail" output="normal" modifiers="" formatter="image/path" />";
@@ -24,10 +27,10 @@ if ($current_section_title != $old_section_title) {
   <a class="no-border" href="<t4 type="navigation" id="21"/>" title="">
     <?php // if user has specified a thumbnail, use that 
     if ($thumbnail_media_path != '') { ?>
-      <figure><img class="m-hide" src="<?php echo $thumbnail_media_path; ?>" alt=""></figure>
+      <figure><img class="m-hide resrc" data-src="http://app.resrc.it/<?php if (environment() == 'live') { echo siteURL(); } ?><?php echo $thumbnail_media_path; ?>"></figure>
     <?php } // otherwise, if an image has been uploaded to the main image field, display that using resrc.it
     elseif ($media_path != '' && $image_or_video == 'image') { ?>
-      <figure><img class="m-hide resrc" data-src="http://app.resrc.it/C=SQ/C=W75P,H75P,XOF50,YOF20/S=W300/<?php if (environment() == 'live') { echo siteURL(); } ?><?php echo $media_path; ?>" alt=""></figure>
+      <figure><img class="m-hide resrc" data-src="http://app.resrc.it/C=SQ/C=W75P,H75P,XOF50,YOF20/S=W300/<?php if (environment() == 'live') { echo siteURL(); } ?><?php echo $media_path; ?>"></figure>
     <?php } // finally, if no other image is available, display a placeholder
     else { ?>
       <t4 type="navigation" id="4309" />
