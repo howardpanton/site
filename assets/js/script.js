761ef449b524e05516da8a3b956f5b58639d2d6a
denovo <pete.richardson82@gmail.com>
2013-11-26 09:06:38 +0000

    update IE indexOf fix in script.js

--- assets/js/script.js
+++ assets/js/script.js
@@ -39,7 +39,7 @@ jQuery.fn.extend({
 // add indexOf for IE8 compatibility
 if (!Array.prototype.indexOf) {
     Array.prototype.indexOf = function (elt /*, from*/) {
-        var len = this.length >>> 0;
+        var len = +this.length || 0;
         var from = Number(arguments[1]) || 0;
         from = (from < 0) ? Math.ceil(from) : Math.floor(from);
         if (from < 0) from += len;
