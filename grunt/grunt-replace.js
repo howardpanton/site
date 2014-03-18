4f7bb34603dd512c1da179a3cfe5d6a239ddfe58
Pete Richardson <pete.richardson82@gmail.com>
2014-03-18 19:49:35 +0000

    updated github path in replace task

--- grunt/grunt-replace.js
+++ grunt/grunt-replace.js
@@ -5,29 +5,21 @@
 
 module.exports = function(grunt) {
 		grunt.config('replace', {
-				siteurl: {
-						src: ['source/_layout/_base.jade', 'source/_includes/_layouts/_head.jade'],
-						overwrite: true, // overwrite matched source files
-						replacements: [{
-							from: "localhost:9000",
-							to: "artslondon.github.io/"
-						}]
-				},
 				local: {
-						src: ['source/_layout/_base.jade', 'source/_includes/_layouts/_head.jade'],
+						src: ['source/_layout/_base.jade', 'source/_includes/_layouts/_head.jade', 'source/_layout/_footer.jade'],
 						overwrite: true,
 						replacements: [{
 							from: "{{site.url}}",
 							to: "http://localhost:9000"
 						},
 						{
-							from: "http://artslondon.github.io/",
+							from: "http://artslondon.github.io/arts.ac.uk/build",
 							to: "http://localhost:9000"
 						}
 						]
 				},
 				github: {
-						src: ['source/_layout/_base.jade', 'source/_includes/_layouts/_head.jade'],
+						src: ['source/_layout/_base.jade', 'source/_includes/_layouts/_head.jade', 'source/_layout/_footer.jade'],
 						overwrite: true,
 						replacements: [{
 							from: "{{site.url}}",
@@ -49,10 +41,10 @@ module.exports = function(grunt) {
 						]
 				},
 				reset: {
-						src: ['source/_layout/_base.jade', 'source/_includes/_layouts/_head.jade'],
+						src: ['source/_layout/_base.jade', 'source/_includes/_layouts/_head.jade', 'source/_layout/_footer.jade' ],
 						overwrite: true,
 						replacements: [{
-							from: "http://artslondon.github.io/",
+							from: "http://artslondon.github.io/arts.ac.uk/build",
 							to: "{{site.url}}"
 						},
 						{
