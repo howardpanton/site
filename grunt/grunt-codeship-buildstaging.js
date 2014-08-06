665c9ff7b39b51fe50ce52fb0191febcb378cd40
denovo <pete.richardson82@gmail.com>
2014-08-06 14:32:44 +0100

    fix issues with codeship build staging task

--- grunt/grunt-codeship-buildstaging.js
+++ grunt/grunt-codeship-buildstaging.js
@@ -12,6 +12,8 @@ module.exports = function(grunt) {
 
 		grunt.registerTask('codeship_build_staging', 'builds CSS and JS files to staging without any prompts', function() {
 
+			// grunt.config('deployTo').set("staging");
+
 			// clear out /build/
 			grunt.task.run('clean:before_build');
 
@@ -31,7 +33,19 @@ module.exports = function(grunt) {
 
 
 			// builstaging
+
 			grunt.log.writeln('Uploading files to AWS staging....');
-			grunt.task.run('go_build_staging');
+			// grunt.task.run('go_build_staging');
+
+			grunt.task.run([  'replace:spritePath',
+												'compress:gzip_css',
+												'compress:gzip_js',
+												'copy:gzipped_css',
+												'copy:gzipped_js',
+												'compress:gzip_image_assets',
+												'copy:gzipped_image_assets:',
+												'aws_s3:staging',
+												'cloudfront_clear'
+				]);
 		});
 };
